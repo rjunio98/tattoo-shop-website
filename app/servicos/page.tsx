b5e2ba31191022486fd26d0b96b2f505763417aa
rjunio98 <robledorjunior1@gmail.com>
2024-05-07 22:02:43 -0300

    feat: páginas criadas

---
+++ app/servicos/page.tsx
@@ -0,0 +1,7 @@
+export default function Servicos() {
+  return (
+    <div>
+      <h1>Página de serviços</h1>
+    </div>
+  );
+}
---
+++ app/sobre/page.tsx
@@ -0,0 +1,7 @@
+export default function Sobre() {
+    return (
+      <div>
+        <h1>Página de sobre</h1>
+      </div>
+    );
+  }
