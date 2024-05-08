6810197e0a546778199b288ba0a0e42957828635
rjunio98 <robledorjunior1@gmail.com>
2024-05-08 20:50:40 -0300

    feat: Update footer and navbar components

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,17 +1,27 @@
 import Image from "next/image";
 import logo from "@/app/_assets/logo.svg";
+import { londrinaSolid } from "../_lib/font";
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav
+      className={`container flex justify-between py-8 ${londrinaSolid.className}`}
+    >
       <Image
         src={logo}
-        alt="Logo"  
+        alt="Logo"
       />
-      <ul>
+      <ul className="flex gap-6 text-3xl">
         <li>Serviços</li>
         <li>Sobre mim</li>
-        <li>Contato (+55 11 99999 9999)</li>
+        <li className="text-right">
+          Contato
+          <span className="text-xs text-zinc-400 font-extralight flex gap-1 items-center">
+            <FaWhatsapp />
+            (+55 11 99999 9999)
+          </span>
+        </li>
       </ul>
     </nav>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,11 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    container: {
+      center: true,
+    },
+  },
   plugins: [],
 };
 export default config;
