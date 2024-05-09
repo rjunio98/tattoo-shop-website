5017ac382fe939029832807c905eaba993adcfd1
rjunio98 <robledorjunior1@gmail.com>
2024-05-08 21:19:49 -0300

    feat: Atualizar componente Navbar com links para as páginas de serviços, sobre e contato

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,28 +1,41 @@
+"use client";
+
 import Image from "next/image";
 import logo from "@/app/_assets/logo.svg";
 import { londrinaSolid } from "../_lib/font";
 import { FaWhatsapp } from "react-icons/fa";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
+  const isActive = (currentPath: string, pathname: string) => {
+    if (currentPath.includes(pathname)) return "underline";
+  };
+
   return (
     <nav
       className={`container flex justify-between py-8 ${londrinaSolid.className}`}
     >
-      <Image
-        src={logo}
-        alt="Logo"
-      />
+      <Link href="/">
+        <Image
+          src={logo}
+          alt="Logo"
+        />
+      </Link>
       <ul className="flex gap-6 text-3xl">
-        <li>
+        <li className={`${isActive(pathname, "servicos")}`}>
           <Link href="/servicos">Serviços</Link>
         </li>
-        <li>
+        <li className={`${isActive(pathname, "sobre")}`}>
           <Link href="/sobre">Sobre mim</Link>
         </li>
-        <li className="text-right">
+        <li>
           <Link href="/contato">
-            Contato
+            <span className={`text-right ${isActive(pathname, "contato")}`}>
+              Contato
+            </span>
             <span className="text-xs text-zinc-400 font-extralight flex gap-1 items-center">
               <FaWhatsapp />
               (+55 11 99999 9999)
